da139d6d7da521eceab9a81ba29ca70ca98f17a6
mseidel819 <mseidel819@gmail.com>
2022-01-26 07:18:18 -0600

    intergrated firebase. added sign ing and sign out component

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+
+const config = {
+  apiKey: "AIzaSyBzUAlrcqEtMDC1zjYfl0A2iUwToz14jwM",
+  authDomain: "crwn-db-28ebb.firebaseapp.com",
+  projectId: "crwn-db-28ebb",
+  storageBucket: "crwn-db-28ebb.appspot.com",
+  messagingSenderId: "153159627475",
+  appId: "1:153159627475:web:34bb4a2be9d49bd30d1a26",
+  measurementId: "G-YQR2YS1R1Y",
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+export default firebase;
